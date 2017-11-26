3193916127d0238f8149be4965f6f504e13e4c6d
lovers jain <loversjain5@gmail.com>
2017-11-26 18:30:05 +0530

    database faker with db:seed

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
-    ];
+        'name'		=>	$faker->name,
+        'details'	=>	$faker->paragraph,
+        'discount'	=>	$faker->numberBetween(2,30),
+        'price'		=>	$faker->numberBetween(100,1000),
+        'stock'		=>	$faker->randomDigit
+    ];	
 });
--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,17 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Model\Product;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id'	=>	function(){
+        					return Product::all()->random();
+        					},
+
+        'star'			=>	$faker->numberBetween(0,5),
+        'reviews'		=>	$faker->paragraph,
+        'customer'		=>	$faker->name,
+
     ];
 });
