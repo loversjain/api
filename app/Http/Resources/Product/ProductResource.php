0040a2a315c5725586c06eec791c1a152dcbce82
lovers jain <loversjain5@gmail.com>
2017-11-27 23:24:10 +0530

    27-11-17

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ProductResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name'  =>  $this->name,
+            'details'   =>$this->details
+        ];
+    }
+}
